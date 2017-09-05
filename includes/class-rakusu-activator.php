56e165504eab9022ae099b858d6cf1825a7578f1
Ramon <Rahmon@users.noreply.github.com>
2017-09-05 18:53:40 -0300

    Initial files

---
+++ includes/class-rakusu-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://github.com/Rahmon
+ * @since      1.0.0
+ *
+ * @package    Rakusu
+ * @subpackage Rakusu/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Rakusu
+ * @subpackage Rakusu/includes
+ * @author     rahmohn <https://github.com/Rahmon/rakusu>
+ */
+class Rakusu_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-rakusu-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/Rahmon
+ * @since      1.0.0
+ *
+ * @package    Rakusu
+ * @subpackage Rakusu/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Rakusu
+ * @subpackage Rakusu/includes
+ * @author     rahmohn <https://github.com/Rahmon/rakusu>
+ */
+class Rakusu_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
